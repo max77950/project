c2c324304c9ce416a2bc9219af014040cb1a79bc
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 14:12:36 +0100

    Update Server.js

--- VS/Server.js
+++ VS/Server.js
@@ -1,16 +1,14 @@
 ﻿var http = require('http');
-var url = require('url');
-var querystring = require('querystring');
 
 var server = http.createServer(function (req, res) {
-    var params = querystring.parse(url.parse(req.url).query);
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    if ('prenom' in params && 'nom' in params) {
-        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
-    }
-    else {
-        res.write('Vous devez bien avoir un prénom et un nom, non ?');
-    }
-    res.end();
+    res.writeHead(200);
+    res.end('Salut tout le monde !');
 });
-server.listen(8080);
+
+server.on('close', function () { // On écoute l'évènement close
+    console.log('Bye bye !');
+})
+
+server.listen(8080); // Démarre le serveur
+
+server.close(); // Arrête le serveur. Déclenche l'évènement close
