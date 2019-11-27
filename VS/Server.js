d6369e046f83bb9447569727828695a245e51f4e
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 21:55:44 +0100

    Update Server.js

--- VS/Server.js
+++ VS/Server.js
@@ -1,20 +1,14 @@
 ﻿var express = require('express');
+var morgan = require('morgan'); // Charge le middleware de logging
+var favicon = require('serve-favicon'); // Charge le middleware de favicon
 
 var app = express();
 
-app.get('/', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes à l\'accueil, que puis-je pour vous ?');
-});
-
-app.get('/sous-sol', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-});
-
-app.get('/etage/1/chambre', function (req, res) {
-    res.setHeader('Content-Type', 'text/plain');
-    res.send('Hé ho, c\'est privé ici !');
-});
+app.use(morgan('combined')) // Active le middleware de logging
+    .use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+    .use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
+    .use(function (req, res) { // Répond enfin
+        res.send('Hello');
+    });
 
 app.listen(8080);
