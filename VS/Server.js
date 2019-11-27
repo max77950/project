492293c3d538c90656bde5c404d7bb5afc60752b
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 22:08:36 +0100

    Update Server.js

--- VS/Server.js
+++ VS/Server.js
@@ -5,7 +5,7 @@ var favicon = require('serve-favicon'); // Charge le middleware de favicon
 var app = express();
 
 app.use(morgan('combined')) // Active le middleware de logging
-    .use(express.static(__dirname + '/public')) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
+    .use(express.static(__dirname )) // Indique que le dossier /public contient des fichiers statiques (middleware chargé de base)
     .use(favicon(__dirname + '/public/favicon.ico')) // Active la favicon indiquée
     .use(function (req, res) { // Répond enfin
         res.send('Hello');
