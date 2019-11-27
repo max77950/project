5821cc03d9e396b53265d11546cbc7733c95b6a5
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 13:40:35 +0100

    Update Server.js

--- VS/Server.js
+++ VS/Server.js
@@ -1,7 +1,17 @@
 ﻿var http = require('http');
 
 var server = http.createServer(function (req, res) {
-    res.writeHead(200);
-    res.end('Salut tout le monde ! website');
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write('<!DOCTYPE html>' +
+        '<html>' +
+        '    <head>' +
+        '        <meta charset="utf-8" />' +
+        '        <title>Ma page Node.js !</title>' +
+        '    </head>' +
+        '    <body>' +
+        '     	<p>Voici un paragraphe <strong>HTML</strong> !</p>' +
+        '    </body>' +
+        '</html>');
+    res.end();
 });
 server.listen(8080);
