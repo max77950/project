f02af4b239fa7fcc372b4f8429643cea75b24955
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 14:03:13 +0100

    Update Server.js

--- VS/Server.js
+++ VS/Server.js
@@ -1,18 +1,15 @@
 ﻿var http = require('http');
 var url = require('url');
+var querystring = require('querystring');
 
 var server = http.createServer(function (req, res) {
-    var page = url.parse(req.url).pathname;
-    console.log(page);
+    var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, { "Content-Type": "text/plain" });
-    if (page == '/') {
-        res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
+    if ('prenom' in params && 'nom' in params) {
+        res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
     }
-    else if (page == '/sous-sol') {
-        res.write('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
-    }
-    else if (page == '/etage/1/chambre') {
-        res.write('Hé ho, c\'est privé ici !');
+    else {
+        res.write('Vous devez bien avoir un prénom et un nom, non ?');
     }
     res.end();
 });
