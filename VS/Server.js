c5845f12dc26a41feb72561c6970b5211b0fc780
max77950 <58215709+max77950@users.noreply.github.com>
2019-11-27 13:27:07 +0100

    Create Server.js

---
+++ VS/Server.js
@@ -0,0 +1,7 @@
+﻿var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    res.end('Salut tout le monde ! website');
+});
+server.listen(8080);
